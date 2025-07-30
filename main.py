a5a9365b39ff1218101c47046ca201344316ce39
Superfly101 <Danielukoha101@gmail.com>
2025-07-30 16:14:56 +0100

    asdfasd

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+def random_greeting():
+    greetings = [
+        "Hello, world!",
+        "Hi there!",
+        "Greetings!",
+        "Howdy!",
+        "Hey!",
+        "Salutations!",
+        "Good day!"
+    ]
+    return random.choice(greetings)
+
+if __name__ == "__main__":
+    print(random_greeting())
